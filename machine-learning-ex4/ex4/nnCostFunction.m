1ddbbf7cced318afe5770ceb7bf1c2c4195cbf66
Abu Asif Khan Chowdhury <abu.chowdhury@selise.ch>
2017-03-18 02:19:00 +0600

    regularization error corrected (First element of theta is not taken into regularization this time)

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -110,51 +110,15 @@ Delta1 = delta2' * A1; %(25x401)
 % Step 4 (Gradient calculation)
 % D2 = (1/m)*Delta2 + (lambda)*Theta2;
 % D1 = (1/m)*Delta1 + (lambda)*Theta1;
-D2 = (1/m)*Delta2 + (lambda/m)*Theta2;
-D1 = (1/m)*Delta1 + (lambda/m)*Theta1;
+
+% (CAUTION! Must not regularize the first elements of Theta)
+D2 = (1/m)*Delta2 + (lambda/m)*[zeros(size(Theta2,1),1) Theta2(:,2:end)];
+D1 = (1/m)*Delta1 + (lambda/m)*[zeros(size(Theta1,1),1) Theta1(:,2:end)];
 
 Theta1_grad = D1;
 Theta2_grad = D2;
 
-%% Test
-% a1 = [ones(m, 1) X];
-% z2 = a1 * Theta1';
-% a2 = sigmoid(z2);
-% a2 = [ones(m, 1) a2];
-% z3 = a2 * Theta2';
-% htheta = sigmoid(z3);
-% 
-% for k = 1:num_labels
-%     yk = y == k;
-%     hthetak = htheta(:, k);
-%     Jk = 1 / m * sum(-yk .* log(hthetak) - (1 - yk) .* log(1 - hthetak));
-%     J = J + Jk;
-% end
-% 
-% regularization = lambda / (2 * m) * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)));
-% J = J + regularization;
-% 
-% 
-% for t = 1:m
-%     for k = 1:num_labels
-%         yk = y(t) == k;
-%         delta_3(k) = htheta(t, k) - yk;
-%     end
-%     delta_2 = Theta2' * delta_3' .* sigmoidGradient([1, z2(t, :)])';
-%     delta_2 = delta_2(2:end);
-% 
-%     Theta1_grad = Theta1_grad + delta_2 * a1(t, :);
-%     Theta2_grad = Theta2_grad + delta_3' * a2(t, :);
-% end
-% 
-% Theta1_grad = Theta1_grad / m;
-% Theta2_grad = Theta2_grad / m;
-% 
-% 
-% Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda / m * Theta1(:, 2:end);
-% Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda / m * Theta2(:, 2:end);
-
-% Unroll gradients
+%% Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 
