33cff264ad883526a200b963988e72de8deef31f
Abu Asif Khan Chowdhury <abu.chowdhury@selise.ch>
2017-04-08 02:54:56 +0600

    gaussian kernel implemented and dataset2Params completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -1,7 +1,7 @@
 function [C, sigma] = dataset3Params(X, y, Xval, yval)
-%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
-%where you select the optimal (C, sigma) learning parameters to use for SVM
-%with RBF kernel
+%   DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%   where you select the optimal (C, sigma) learning parameters to use for SVM
+%   with RBF kernel
 %   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
 %   sigma. You should complete this function to return the optimal C and 
 %   sigma based on a cross-validation set.
@@ -23,7 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+Prediction = 100000;
 
+values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+for Ci = values
+    for sigmai = values
+        model = svmTrain(X, y, Ci, @(x1,x2) gaussianKernel(x1,x2, sigmai));
+        predictions = svmPredict(model, Xval);
+        prediction = mean(double(predictions~=yval));
+        if(Prediction>prediction)
+            C = Ci;
+            sigma = sigmai;
+            Prediction = prediction;
+        end
+    end
+end
 
 
 
--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -4,7 +4,7 @@ function sim = gaussianKernel(x1, x2, sigma)
 %   and returns the value in sim
 
 % Ensure that x1 and x2 are column vectors
-x1 = x1(:); x2 = x2(:);
+x1 = x1(:); x2 = x2(:); % (3x1)
 
 % You need to return the following variables correctly.
 sim = 0;
@@ -16,9 +16,7 @@ sim = 0;
 %
 %
 
-
-
-
+sim = exp( - norm(x1 - x2) .^2 / (2 * sigma .^2 ));
 
 
 % =============================================================
